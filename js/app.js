95c6d82921dbc2e0e37168431a59dd4bba77dc99
John O'connor <thien.hieu2299@gmail.com>
2023-05-27 23:10:09 +0700

    add translatez

--- js/app.js
+++ js/app.js
@@ -6,11 +6,22 @@ window.addEventListener("mousemove", (e) => {
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY - window.innerHeight / 2;
 
+
+
     parallax_el.forEach((el) => {
         let speedx = el.dataset.speedx;
         let speedy = el.dataset.speedy;
 
-        el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px))`;
+        let isInLeft = parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
+        
+        let zValue = (e.clientX - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
+        
+        el.style.transform = `
+        translateX(calc(-50% + ${-xValue * speedx}px)) 
+        translateY(calc(-50% + ${yValue * speedy}px)) 
+        perspective(2300px) 
+        translateZ(${zValue}px) 
+        `;
     })
 
 })
