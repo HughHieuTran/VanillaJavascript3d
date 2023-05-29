c99519a309f68ab5f0fab9e788e5f5e29a53cfec
unknown <thien.hieu2299@gmail.com>
2023-05-29 09:52:22 +0700

    add initial animation

--- js/app.js
+++ js/app.js
@@ -1,27 +1,84 @@
 const parallax_el = document.querySelectorAll(".parallax");
 
-let xValue = 0, yValue = 0;
+let xValue = 0, yValue = 0, rotateDegree = 0;
 
-window.addEventListener("mousemove", (e) => {
-    xValue = e.clientX - window.innerWidth / 2;
-    yValue = e.clientY - window.innerHeight / 2;
-
-
-
+const update = (cursorPosition) => {
     parallax_el.forEach((el) => {
         let speedx = el.dataset.speedx;
         let speedy = el.dataset.speedy;
-
+        let rotationSpeed = el.dataset.rotation;
         let isInLeft = parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
-        
-        let zValue = (e.clientX - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
-        
+
+        let zValue = (cursorPosition - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
+
         el.style.transform = `
         translateX(calc(-50% + ${-xValue * speedx}px)) 
         translateY(calc(-50% + ${yValue * speedy}px)) 
         perspective(2300px) 
         translateZ(${zValue}px) 
+        rotateY(${rotateDegree * rotationSpeed}deg)
         `;
     })
+}
+
+update(0);
+
+window.addEventListener("mousemove", (e) => {
+    if (timeline.isActive()) return;
+
+    xValue = e.clientX - window.innerWidth / 2;
+    yValue = e.clientY - window.innerHeight / 2;
+
+    rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
+
+    update(e.clientX);
 
 })
+
+let timeline = gsap.timeline();
+
+Array.from(parallax_el)
+    .filter(el => !el.classList.contains("text"))
+    .forEach((el) => {
+
+        timeline.from(
+            el,
+            {
+                top: `${(el.offsetHeight / 2) + +el.dataset.distance}px`,
+                duration: 1
+            }
+            , "1"
+        );
+    })
+
+timeline.from(
+    ".text h1",
+    {
+        y: window.innerHeight -
+            document.querySelector(".text h1").getBoundingClientRect().top + 200,
+        duration: 1
+    },
+    "3"
+).from(".text h2",
+    {
+        y: -150,
+        opacity: 0,
+        duration: 1.5,
+    }
+    , "3"
+).from(".hide",
+    {
+        opacity: 0,
+        duration: 1.5
+    },
+    "3"
+)
+
+// timeline.from(
+//     ".bg-img",
+//     {
+//         top: `${document.querySelector(".bg-img").offsetHeight / 2 - 200}px`,
+//         duration: 1
+//     }
+//     , "1"
+// );
